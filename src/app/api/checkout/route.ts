e2272f07371fc60659661396500c27569cc38da8
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-09-25 10:34:14 +0600

    booking success

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         },
       ],
       mode: "payment",
-      success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${origin}/booking-success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/cancel`,
     });
 
