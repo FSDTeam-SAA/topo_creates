324308dcee0b0b93f958f37f3fc35761e6df8736
jarifMohammed <mohammedjarif@protonmail.com>
2026-01-18 23:49:38 +0600

    Refactor middleware and auth configuration by commenting out unused code

--- src/auth.ts
+++ src/auth.ts
@@ -79,15 +79,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
   trustHost: true,
-  cookies: {
-    sessionToken: {
-      name: process.env.NODE_ENV === 'production' ? `__Secure-next-auth.session-token` : 'next-auth.session-token',
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
-    },
-  },
+  // cookies: {
+  //   sessionToken: {
+  //     name: process.env.NODE_ENV === 'production' ? `__Secure-next-auth.session-token` : 'next-auth.session-token',
+  //     options: {
+  //       httpOnly: true,
+  //       sameSite: 'lax',
+  //       path: '/',
+  //       secure: process.env.NODE_ENV === 'production',
+  //     },
+  //   },
+  // },
 })
