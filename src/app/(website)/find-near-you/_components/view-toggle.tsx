48d0246925189b057bbea1f956452e0cb3336b13
jarifMohammed <mohammedjarif@protonmail.com>
2026-01-27 11:25:16 +0600

    refactor: Standardize styling across components using brand-prefixed utility classes.

--- src/app/(website)/find-near-you/_components/view-toggle.tsx
+++ src/app/(website)/find-near-you/_components/view-toggle.tsx
@@ -8,7 +8,7 @@ export default function ViewToggle() {
   const isMapView = pathname === "/find-near-you/map";
 
   return (
-    <div className="flex justify-end mb-4 brand-body text-black uppercase">
+    <div className="flex justify-center mb-4 brand-body text-black uppercase">
       <Link
         href="/find-near-you"
         className={`mr-4 ${!isMapView ? "border-b border-black pb-1" : ""
